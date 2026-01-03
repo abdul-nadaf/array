e72b556b421f0e3841a15d2ec1dadb49222fe950
ShivamSloth <shivamyallurkar312@gmail.com>
2026-01-03 12:28:05 +0530

    modified program

---
+++ array.py
@@ -0,0 +1,10 @@
+def main():
+    score =[78,85,90,66,88]
+    total=sum(score)
+    average=total/len(score)
+    print("===main/master branch output===")
+    print(f"score: {score}")
+    print(f"sum: {total}")
+    print(f"average: {average}")
+if __name__ == "__main__":
+    main()
